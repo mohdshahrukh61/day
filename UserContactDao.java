7909d5abfb09f724f820a76ead2da4d0691ef8f0
mohdshahrukh61 <88145929+mohdshahrukh61@users.noreply.github.com>
2023-09-06 05:22:25 +0530

    Add files via upload

---
+++ UserContactDao.java
@@ -0,0 +1,10 @@
+package com.example.day.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.day.entity.UserContact;
+
+public interface UserContactDao extends JpaRepository< UserContact, Long>{
+
+}
+ 
